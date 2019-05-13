0ecdd8c3ef6bfa42a5983f0813cf7730b7e514ac
truongbt <bui.tuan.truong@framgia.com>
2019-05-13 16:36:58 +0700

    remove file log info

--- app/logger/winston.js
+++ app/logger/winston.js
@@ -4,18 +4,8 @@ const { createLogger, format, transports } = require('winston');
 const { combine, errors, json, timestamp, splat, printf } = format;
 
 const options = {
-  file_error: {
-    level: 'error',
+  file: {
     filename: `${__dirname}/error.log`,
-    handleExceptions: true,
-    json: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-    colorize: false,
-  },
-  file_info: {
-    level: 'info',
-    filename: `${__dirname}/info.log`,
     handleExceptions: true,
     json: true,
     maxsize: 5242880, // 5MB
@@ -40,7 +30,7 @@ const logger = createLogger({
     json(),
     printf(info => `${info.timestamp} ${info.level}: ${info.message}${info.stack ? ' - ' + info.stack : ''}`)
   ),
-  transports: [new transports.File(options.file_error), new transports.File(options.file_info)],
+  transports: [new transports.File(options.file)]
 });
 
 if (process.env.NODE_ENV !== 'production') {
