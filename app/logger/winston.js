bfa072d64b2793856dfd0df1ecd5dd7eb7e47c03
truongbt <bui.tuan.truong@framgia.com>
2019-05-14 09:25:15 +0700

    add config winston

--- app/logger/winston.js
+++ app/logger/winston.js
@@ -2,39 +2,33 @@
 
 const { createLogger, format, transports } = require('winston');
 const { combine, errors, json, timestamp, splat, printf } = format;
+const dailyRotateFile = require('winston-daily-rotate-file');
 
-const options = {
-  file: {
-    filename: `${__dirname}/error.log`,
-    handleExceptions: true,
-    json: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-    colorize: false,
-  },
-  console: {
-    level: 'debug',
-    handleExceptions: true,
-    json: false,
-    colorize: true,
+const config = require('./config');
+
+module.exports = {
+  init: (channel = 'error') => {
+    if (typeof config.transports[channel] === 'undefined') {
+      return `Config channel ${channel} not found`;
+    }
+
+    let logger = createLogger({
+      format: combine(
+        timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        errors({ stack: true }),
+        splat(),
+        json(),
+        printf(info => `${info.timestamp} ${info.level}: ${info.message}${info.stack ? ' - ' + info.stack : ''}`)
+      ),
+      transports: [new dailyRotateFile(config.transports[channel])],
+    });
+
+    if (process.env.NODE_ENV !== 'production') {
+      logger.add(new transports.Console(config.transports.console));
+    }
+
+    return logger;
   },
 };
-
-const logger = createLogger({
-  format: combine(
-    timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    errors({ stack: true }),
-    splat(),
-    json(),
-    printf(info => `${info.timestamp} ${info.level}: ${info.message}${info.stack ? ' - ' + info.stack : ''}`)
-  ),
-  transports: [new transports.File(options.file)]
-});
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.Console(options.console));
-}
-
-module.exports = logger;
