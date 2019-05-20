21df19232e84fd85483f09a8eebf182a9c842702
thaipt1012 <thaipt1012@gmail.com>
2019-05-20 14:09:30 +0700

    update model room

--- app/models/room.js
+++ app/models/room.js
@@ -1,67 +1,83 @@
 const mongoose = require('mongoose');
+const config = require('../../config/config');
 
 const Schema = mongoose.Schema;
 // Setup schema
-const Messages = new Schema({
+const Messages = new Schema(
+  {
     content: { type: String },
     user: { type: Schema.ObjectId, ref: 'User' },
-    deletedAt: { type: Date, default: null }
-}, {
+    deletedAt: { type: Date, default: null },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Members = new Schema({
+const Members = new Schema(
+  {
     user: { type: Schema.ObjectId, ref: 'User' },
-    role: { type: Number, default: 1 }, //0: admin - 1: member - 2: read-only
-    last_message_id: { type: Number, default: null },
+    role: { type: Number, default: config.MEMBER_ROLE.MEMBER }, //0: admin - 1: member - 2: read-only
+    last_message_id: { type: Schema.ObjectId, ref: 'Messages' },
     marked: { type: Boolean, default: false },
     room_group: { type: Schema.ObjectId, ref: 'Room_group' },
-    deletedAt: { type: Date, default: null }
-}, {
+    deletedAt: { type: Date, default: null },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Tasks = new Schema({
+const Tasks = new Schema(
+  {
     content: { type: String },
     due: { type: Date, default: null },
     assignees: { type: Schema.ObjectId, ref: 'User' },
     done: { type: Boolean, default: false },
-    deletedAt: { type: Date, default: null }
-}, {
+    deletedAt: { type: Date, default: null },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Files = new Schema({
+const Files = new Schema(
+  {
     name: { type: String },
     path: { type: String },
-    deletedAt: { type: Date, default: null }
-}, {
+    deletedAt: { type: Date, default: null },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const RoomSchema = new Schema({
+const RoomSchema = new Schema(
+  {
     name: { type: String },
     desc: { type: String },
-    type: { type: Number, defauls: 0 }, //0: group chat - 1: direct chat
+    type: { type: Number, defauls: config.ROOM_TYPE.GROUP_CHAT }, //0: group chat - 1: direct chat
     invitation_code: { type: String },
-    invitation_type: { type: Number, default: 1 }, //0: need admin approves - 1: don't need admin approves
+    invitation_type: { type: Number, default: config.INVITATION_TYPE.NOT_NEED_APPROVAL }, //0: don't need admin approves - 1: need admin approves
     avatar_url: { type: String },
     members: [Members],
     messages: [Messages],
     tasks: [Tasks],
     files: [Files],
-    deletedAt: { type: Date, default: null }
-}, {
+    deletedAt: { type: Date, default: null },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
 RoomSchema.statics = {
-    load: function(options, cb) {
-        options.select = options.select || 'name';
-        return this.findOne(options.criteria)
-            .select(options.select)
-            .exec(cb);
-    },
-}
+  load: function(options, cb) {
+    options.select = options.select || 'name';
+    return this.findOne(options.criteria)
+      .select(options.select)
+      .exec(cb);
+  },
+};
 
 module.exports = mongoose.model('Room', RoomSchema);
--- config/config.js
+++ config/config.js
@@ -1,4 +1,18 @@
 module.exports = {
   CLIENT_ORIGIN: process.env.NODE_ENV === 'production' ? process.env.CLIENT_ORIGIN : 'http://localhost:3000',
   LIMIT_ITEM_SHOW: 30,
+  MEMBER_ROLE: {
+    ADMIN: 0,
+    MEMBER: 1,
+    READ_ONLY: 2,
+  },
+  ROOM_TYPE: {
+    GROUP_CHAT: 0,
+    DIRECT_CHAT: 1,
+  },
+  INVITATION_TYPE: {
+    NOT_NEED_APPROVAL: 0,
+    NEED_APPROVAL: 1,
+    CANNOT_REQUEST: 2,
+  },
 };
