ad4d3c8e43ad4382ffb773336c8eb5910f873b66
Tuan Pham <pham.thanh.tuan@sun-asterisk.com>
2019-06-10 17:42:22 +0700

    Update client socket config

--- client/src/config/configServer.js
+++ client/src/config/configServer.js
@@ -1,6 +1,6 @@
 const config = {
   API_URL: process.env.REACT_APP_API_URL,
-  SOCKET_ENDPOINT: process.env.SOCKET_ENDPOINT,
+  SOCKET_ENDPOINT: process.env.REACT_APP_SOCKET_ENDPOINT,
   LOCALE: process.env.REACT_APP_LOCALE || 'en',
 };
 
