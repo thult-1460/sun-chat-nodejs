38bda107c24282c2d64abea21e99fa721da3408a
sangct-1630 <cao.thanh.sang@sun-asterisk.com>
2019-07-10 14:45:04 +0700

    fix render html from block

--- client/src/helpers/handlersMessage.js
+++ client/src/helpers/handlersMessage.js
@@ -5,7 +5,6 @@ import i18n from '../i18n';
 
 const _ = require('lodash');
 
-let contentHtml = '';
 let listMembers = [];
 let subContent = {};
 
@@ -31,15 +30,7 @@ const actionFunc = {
     return false;
   },
   replyMember: function(e) {
-    insertTextToMessageArea(
-      '[' +
-        i18n.t('message:button.reply') +
-        ' mid=' +
-        e.target.getAttribute('data-mid') +
-        '] ' +
-        e.target.getAttribute('data-name') +
-        '\n'
-    );
+    insertTextToMessageArea('[' + i18n.t('message:button.reply') + ' mid=' + e.target.getAttribute('data-mid') + '] ' + e.target.getAttribute('data-name') + '\n');
 
     return false;
   },
@@ -54,33 +45,29 @@ const getAvatarByID = (id, userInfo) => {
   let member = listMembers.find(member => member._id == id);
   let avatar = member && member.avatar ? getUserAvatarUrl(member.avatar) : messageConfig.ICO_AVATAR_NOTFOUND;
 
-  return `<img data-mid="${id}" src="${
-    userInfo[id] && userInfo[id].avatar ? getUserAvatarUrl(userInfo[id].avatar) : avatar
-  }" onError="this.onerror=null;this.src='${messageConfig.ICO_AVATAR_NOTFOUND}';" />`;
+  return `<img data-mid="${id}" src="${userInfo[id] && userInfo[id].avatar ? getUserAvatarUrl(userInfo[id].avatar) : avatar}" onError="this.onerror=null;this.src='${messageConfig.ICO_AVATAR_NOTFOUND}';" />`;
 };
 
 const messageToHtml = {
   to: function(id, userInfo) {
     let avatar = getAvatarByID(id, userInfo);
 
-    return `<div data-tag="[To:${id}]" class="messageBadge" contenteditable="false"><div class="chatTimeLineTo"><span>To</span>${avatar}</div></div>`;
+    return `<div class="messageBadge"><div class="chatTimeLineTo"><span>To</span>${avatar}</div></div>`;
   },
   code: function(code) {
     let codeHighlight = hljs.highlightAuto(_.unescape(code));
 
-    return `<code data-tag="[code] ${code} [/code]" class="chatCode">${codeHighlight.value}</code>`;
+    return `<code class="chatCode">${codeHighlight.value}</code>`;
   },
   toall: function() {
     return (
-      '<div class="messageBadge"' +
-      messageConfig.SIGN_TO_ALL +
-      '><div class="messageBadge__toAllBadge" contenteditable="false"><span>TO ALL</span></div></div>'
+      '<div class="messageBadge"' + messageConfig.SIGN_TO_ALL + '><div class="messageBadge__toAllBadge"><span>TO ALL</span></div></div>'
     );
   },
   reply: function(memberId, userInfo) {
     let avatar = getAvatarByID(memberId, userInfo);
 
-    return `<div data-tag="[rp mid=${memberId}]" class="messageBadge" contenteditable="false"><div class="chatTimeLineTo" data-mid="${memberId}"><span>&#8592; Re</span>${avatar}</div></div>`;
+    return `<div class="messageBadge"><div class="chatTimeLineTo" data-mid="${memberId}"><span>&#8592; Re</span>${avatar}</div></div>`;
   },
   title: function(content) {
     return `<div><b>&#9432</b> ${content}</div>`;
@@ -91,39 +78,40 @@ const renderMessageToHtml = {
   to: function(content, userInfo) {
     let regEx = /\[to:([\w-]+)\]/gi;
     let match = regEx.exec(content);
-    let tmpMatch = {};
 
     while (match !== null) {
-      tmpMatch[match[0]] = messageToHtml.to(match[1], userInfo);
+      content = content.replace(match[0], messageToHtml.to(match[1], userInfo));
       match = regEx.exec(content);
     }
 
-    _.map(tmpMatch, function(el, key) {
-      contentHtml = contentHtml.replace(new RegExp(_.escapeRegExp(key), 'g'), el);
-    });
-
-    return false;
+    return content;
   },
   toall: function(content, userInfo) {
-    contentHtml = contentHtml.replace(/\[toall\]/gi, messageToHtml.toall());
+    content = content.replace(/\[toall\]/gi, messageToHtml.toall());
 
-    return false;
+    return content;
   },
   reply: function(content, userInfo) {
     let regEx = /\[rp mid=([\w-]+).*?\]/g;
     let match = regEx.exec(content);
-    let tmpMatch = {};
 
     while (match !== null) {
-      tmpMatch[match[0]] = messageToHtml.reply(match[1], userInfo);
+      content = content.replace(match[0], messageToHtml.reply(match[1], userInfo));
       match = regEx.exec(content);
     }
 
-    _.map(tmpMatch, function(el, key) {
-      contentHtml = contentHtml.replace(new RegExp(_.escapeRegExp(key), 'g'), el);
-    });
+    return content;
+  },
+  title: function(content, userInfo) {
+    let regEx = /(\[title\])(((?!\[(title|\/title)\]).)*)(\[\/title\])/s;
+    let match = regEx.exec(content);
 
-    return false;
+    while (match !== null) {
+      content = content.replace(match[0], messageToHtml.title(match[2]));
+      match = regEx.exec(content);
+    }
+
+    return content;
   },
 };
 
@@ -148,9 +136,7 @@ const handleBlockCode = content => {
   let tmpContent = content;
 
   while (match !== null) {
-    let key = `%_${Math.random()
-      .toString(36)
-      .substring(2, 35)}${new Date().getTime()}_%`;
+    let key = `%_${Math.random().toString(36).substring(2, 35)}${new Date().getTime()}_%`;
     let blockCode = renderBlockCode(match[0]); // handle blockCode
     subContent[key] = blockCode;
 
@@ -159,18 +145,6 @@ const handleBlockCode = content => {
   }
 
   return tmpContent;
-};
-
-const handleTitle = content => {
-  let regEx = /(\[title\])(((?!\[(title|\/title)\]).)*)(\[\/title\])/s;
-  let match = regEx.exec(content);
-
-  while (match !== null) {
-    content = content.replace(match[0], messageToHtml.title(match[2]));
-    match = regEx.exec(content);
-  }
-
-  return content;
 };
 
 const renderMessage = (message, members, userInfo = {}) => {
@@ -182,18 +156,16 @@ const renderMessage = (message, members, userInfo = {}) => {
 
   listMembers = members;
   content = _.escape(handleBlockCode(content));
-  content = handleTitle(content);
 
-  contentHtml = content;
   for (let key in renderMessageToHtml) {
-    renderMessageToHtml[key](content, userInfo);
+    content = renderMessageToHtml[key](content, userInfo);
   }
 
   for (let key in subContent) {
-    contentHtml = contentHtml.replace(key, subContent[key]);
+    content = content.replace(key, subContent[key]);
   }
 
-  return contentHtml;
+  return content;
 };
 
 export default {
