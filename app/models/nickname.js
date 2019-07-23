7babf7c7fd69b06c433eaed4decb0f5593a8fbb8
lethu96 <lethucntt1@gmail.com>
2019-07-23 09:50:11 +0700

    show nick name in list member

---
+++ app/models/nickname.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * NickName Schema
+ */
+
+const NickNameSchema = new Schema(
+  {
+    owner: { type: Schema.ObjectId, ref: 'User', },
+    user_id: { type: Schema.ObjectId, ref: 'User', },
+    nick_name: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    room_id: { type: Schema.ObjectId, ref: 'Room', default: null },
+    deletedAt: { type: Date, default: null },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+module.exports = mongoose.model('NickName', NickNameSchema);
