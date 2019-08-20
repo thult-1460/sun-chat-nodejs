82deef13c4a34285c3b11ffe3e122c231096aa05
FRAMGIA\nguyen.van.minhb <minhnv2306>
2019-08-20 15:47:47 +0700

    fixbug: show token invalid when reset password

--- client/src/pages/auth/resetpassword.js
+++ client/src/pages/auth/resetpassword.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import 'antd/dist/antd.css';
-import { resetPassword } from './../../api/auth.js';
+import { resetPassword, checkResetPasswordToken } from './../../api/auth.js';
 import { Form, Icon, Input, Button, Alert } from 'antd';
 import queryString from 'query-string';
 import { PrivateIcon } from './../../components/PrivateIcon';
@@ -14,6 +14,7 @@ class ResetPassword extends React.Component {
     message: '',
     error: '',
     errors: {},
+    isTokenValid: true,
   };
 
   rules = {
@@ -56,6 +57,17 @@ class ResetPassword extends React.Component {
     },
   };
 
+  componentDidMount() {
+    const url = this.props.location.search;
+    const params = queryString.parse(url);
+    const { token } = params;
+
+    checkResetPasswordToken(token).catch(err => {
+      this.setState({
+        isTokenValid: false,
+      });
+    });
+  }
   handleResetPassword = e => {
     const url = this.props.location.search;
     const params = queryString.parse(url);
@@ -85,93 +97,97 @@ class ResetPassword extends React.Component {
   };
 
   render() {
-    const { message, errors, error } = this.state;
+    const { message, errors, error, isTokenValid } = this.state;
     const { form, t } = this.props;
 
     return (
       <Fragment>
         <div className="form">
-          <Form className="login-form">
-            <h2 className="logo">
-              <Icon style={{ fontSize: 100, color: '#40A9FF' }} theme="outlined" component={PrivateIcon} />
-            </h2>
-            {error ? (
-              <Form.Item>
-                <Alert message="Error" description={error} type="error" showIcon />
-              </Form.Item>
-            ) : (
-              ''
-            )}
-            {typeof message === 'string' && message !== '' ? (
-              <div>
+          {isTokenValid ? (
+            <Form className="login-form">
+              <h2 className="logo">
+                <Icon style={{ fontSize: 100, color: '#40A9FF' }} theme="outlined" component={PrivateIcon} />
+              </h2>
+              {error ? (
                 <Form.Item>
-                  <Alert description={message} type="success" showIcon />
+                  <Alert message="Error" description={error} type="error" showIcon />
                 </Form.Item>
-                <Form.Item>
-                  <a href="/login">{t('login')}</a>
-                </Form.Item>
-              </div>
-            ) : (
-              <div>
-                <Form.Item
-                  help={
-                    form.getFieldError('password') ? (
-                      form.getFieldError('password')
-                    ) : errors && errors.password ? (
-                      <span className="error-message-from-server">{errors.password}</span>
-                    ) : (
-                      ''
-                    )
-                  }
-                >
-                  {form.getFieldDecorator('password', this.rules.password)(
-                    <Input.Password
-                      prefix={<Icon type="lock" style={{ fontSize: 13 }} />}
-                      type="password"
-                      placeholder={t('password')}
-                    />
-                  )}
-                </Form.Item>
-                <Form.Item
-                  help={
-                    form.getFieldError('password_confirmation') ? (
-                      form.getFieldError('password_confirmation')
-                    ) : errors && errors.password_confirmation ? (
-                      <span className="error-message-from-server">{errors.password_confirmation}</span>
-                    ) : (
-                      ''
-                    )
-                  }
-                >
-                  {form.getFieldDecorator('password_confirmation', this.rules.password_confirmation)(
-                    <Input.Password
-                      prefix={<Icon type="lock" style={{ fontSize: 13 }} />}
-                      type="password"
-                      placeholder={t('password_confirmation')}
-                    />
-                  )}
-                </Form.Item>
-                <Form.Item>
-                  <Button
-                    className="login-form-button"
-                    type="primary"
-                    value="Log in"
-                    onClick={this.handleResetPassword}
+              ) : (
+                ''
+              )}
+              {typeof message === 'string' && message !== '' ? (
+                <div>
+                  <Form.Item>
+                    <Alert description={message} type="success" showIcon />
+                  </Form.Item>
+                  <Form.Item>
+                    <a href="/login">{t('login')}</a>
+                  </Form.Item>
+                </div>
+              ) : (
+                <div>
+                  <Form.Item
+                    help={
+                      form.getFieldError('password') ? (
+                        form.getFieldError('password')
+                      ) : errors && errors.password ? (
+                        <span className="error-message-from-server">{errors.password}</span>
+                      ) : (
+                        ''
+                      )
+                    }
                   >
-                    {t('reset_password')}
-                  </Button>
-                  <div>
-                    <a className="login-form-forgot" href="/register">
-                      {t('register')}
-                    </a>
-                    <a style={{ float: 'right' }} href="/login">
-                      {t('login')}
-                    </a>
-                  </div>
-                </Form.Item>
-              </div>
-            )}
-          </Form>
+                    {form.getFieldDecorator('password', this.rules.password)(
+                      <Input.Password
+                        prefix={<Icon type="lock" style={{ fontSize: 13 }} />}
+                        type="password"
+                        placeholder={t('password')}
+                      />
+                    )}
+                  </Form.Item>
+                  <Form.Item
+                    help={
+                      form.getFieldError('password_confirmation') ? (
+                        form.getFieldError('password_confirmation')
+                      ) : errors && errors.password_confirmation ? (
+                        <span className="error-message-from-server">{errors.password_confirmation}</span>
+                      ) : (
+                        ''
+                      )
+                    }
+                  >
+                    {form.getFieldDecorator('password_confirmation', this.rules.password_confirmation)(
+                      <Input.Password
+                        prefix={<Icon type="lock" style={{ fontSize: 13 }} />}
+                        type="password"
+                        placeholder={t('password_confirmation')}
+                      />
+                    )}
+                  </Form.Item>
+                  <Form.Item>
+                    <Button
+                      className="login-form-button"
+                      type="primary"
+                      value="Log in"
+                      onClick={this.handleResetPassword}
+                    >
+                      {t('reset_password')}
+                    </Button>
+                    <div>
+                      <a className="login-form-forgot" href="/register">
+                        {t('register')}
+                      </a>
+                      <a style={{ float: 'right' }} href="/login">
+                        {t('login')}
+                      </a>
+                    </div>
+                  </Form.Item>
+                </div>
+              )}
+            </Form>
+          ) : (
+            <h1> {t('validate.token.in_valid')} </h1>
+          )}
         </div>
       </Fragment>
     );
